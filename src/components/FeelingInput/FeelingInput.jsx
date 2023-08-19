209cff327f4e51b9e734d741b8069a19b10d8ece
LazyDotExe <papaporo@proton.me>
2023-08-18 20:31:43 -0500

    added dispatch info

--- src/components/FeelingInput/FeelingInput.jsx
+++ src/components/FeelingInput/FeelingInput.jsx
@@ -1,16 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
 
 
 
 
 function FeelingInput() {
     const history = useHistory();
+    const dispatch = useDispatch()
+
+    const [addInput, setAddInput] = useState('');
+
     
 
     function handleNext(event) {
         event.preventDefault();
         console.log('inside handleNext')
+        dispatch({
+            type:"ADD_FEEBACK",
+            payload:addInput
+
+        })
+
+
         history.push("/understanding");
     }
 
@@ -18,7 +31,7 @@ function FeelingInput() {
     <h1>How are you feeling today?</h1>
     <form onSubmit={(event) => handleNext(event)}>
       <label>Feeling?</label>
-      <input type="text" placeholder='1-5'required/>
+      <input type="text" placeholder='1-5'required onChange={setAddInput}/>
 
       
 
--- src/index.js
+++ src/index.js
@@ -6,41 +6,34 @@ import { Provider } from "react-redux";
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import logger from "redux-logger";
 
-
-
 const feedback = (state = [], action) => {
-    if (action.type === "ADD_FEEBACK") {
-      return [...state, action.payload];
-    }
-  
-    if (action.type === "FEEDBACK") {
-        const removePizza = state.filter((pizza) => pizza.id !== action.payload.id)
-      return removePizza;
-    }
-  
-    if (action.type === "FEEDBACK") {
-      return [];
-    }
-    return state;
-  };
+  if (action.type === "ADD_FEEBACK") {
+    return [...state, action.payload];
+  }
 
+  if (action.type === "ADD_FEEBACK") {
+    const removePizza = state.filter((pizza) => pizza.id !== action.payload.id);
+    return removePizza;
+  }
 
-  const storeInstance = createStore(
-    combineReducers({
-        feedback,
-      
-    }),
-    applyMiddleware(logger)
-  );
+  if (action.type === "REMOVE_FEEBACK") {
+    return [];
+  }
+  return state;
+};
 
-
-
-
+const storeInstance = createStore(
+  combineReducers({
+    feedback,
+  }),
+  applyMiddleware(logger)
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={storeInstance}></Provider>
-    <App />
+    <Provider store={storeInstance}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
